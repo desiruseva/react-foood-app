d7c5fb5e935fb75753a427c7ef32530b57717d23
Desislava Ruseva <84246146+desiruseva@users.noreply.github.com>
2024-04-02 22:47:06 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,85 +1,25 @@
-// import React from "react";
+import React from "react";
 
-// export const Navbar = () => {
-//     return (
-//         <header>
-//             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//                 <div className="container-fluid">
-//                     {/* <NavLink className="navbar-brand" to="/">CRUD APP</NavLink> */}
-//                     <a class="navbar-brand" href="/">
-//                         Food App
-//                     </a>
-//                     <button
-//                         class="navbar-toggler"
-//                         type="button"
-//                         data-toggle="collapse"
-//                         data-target="#navbarSupportedContent"
-//                         aria-controls="navbarSupportedContent"
-//                         aria-expanded="false"
-//                         aria-label="Toggle navigation"
-//                     >
-//                         <span class="navbar-toggler-icon"></span>
-//                     </button>
-//                     <button
-//                         className="navbar-toggler"
-//                         type="button"
-//                         data-bs-toggle="collapse"
-//                         data-bs-target="#navbarSupportedContent"
-//                         aria-controls="navbarSupportedContent"
-//                         aria-expanded="false"
-//                         aria-label="Toggle navigation"
-//                     >
-//                         <span className="navbar-toggler-icon"></span>
-//                     </button>
-//                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//                             <li className="nav-item">
-//                                 <a className="nav-link active" aria-current="page" href="/">
-//                                     Home
-//                                 </a>
-//                             </li>
-//                         </ul>
-//                         <form className="d-flex">
-//                             <input
-//                                 className="form-control me-2"
-//                                 type="search"
-//                                 placeholder="Search"
-//                                 aria-label="Search"
-//                             />
-//                             <button className="btn btn-outline-success" type="submit">
-//                                 Search
-//                             </button>
-//                         </form>
-//                     </div>
-//                 </div>
-//             </nav>
-//         </header>
-//     );
-// };
-
-
-import React, { useState } from "react";
-
-
-export const Navbar = ({ foodData }) => {
-    const [searchQuery, setSearchQuery] = useState("");
-
-    const handleSearchInputChange = (event) => {
-        setSearchQuery(event.target.value);
-    };
-
-    // Filter food data based on search query
-    const filteredFoodData = foodData?.filter((food) =>
-        food.description.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-
+export const Navbar = () => {
     return (
         <header>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">
+                    {/* <NavLink className="navbar-brand" to="/">CRUD APP</NavLink> */}
+                    <a class="navbar-brand" href="/">
                         Food App
                     </a>
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                     <button
                         className="navbar-toggler"
                         type="button"
@@ -105,10 +45,10 @@ export const Navbar = ({ foodData }) => {
                                 type="search"
                                 placeholder="Search"
                                 aria-label="Search"
-                                value={searchQuery}
-                                onChange={handleSearchInputChange}
                             />
-                            {/* No need for a submit button, search is live */}
+                            <button className="btn btn-outline-success" type="submit">
+                                Search
+                            </button>
                         </form>
                     </div>
                 </div>
@@ -116,11 +56,3 @@ export const Navbar = ({ foodData }) => {
         </header>
     );
 };
-
-export default Navbar;
-
-
-
-
-
-
